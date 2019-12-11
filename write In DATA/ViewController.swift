8592a99b82b46db64fd7728113cfce74ee9c4194
ufoxcode <ufoxcode@gmail.com>
2019-12-11 23:32:11 +0800

    Initial Commit

---
+++ write In DATA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  write In DATA
+//
+//  Created by UFO Xcode on 2019/12/11.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
